ee347bb31b4849f33c7c0cb3873c867eb6264c6e
chatblanc-ciel <matsumoto9845@spice.ocn.ne.jp>
2022-04-13 18:12:38 +0900

    Add remix backtrace function

---
+++ test/stack_backtrace_operation/stack_backtrace_operation_test.cpp
@@ -0,0 +1,42 @@
+//!
+//!
+//!
+
+
+#include "stack_backtrace_operation"
+
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+// using namespace dsp_work_utils;
+
+void run_basic_backtrace();
+
+int main( int argc, char** argv )
+{
+    vector< string > args( argv, argv + argc );
+
+    if ( args.at( 1 ) == string( "basic_backtrace" ) )
+    {
+        run_basic_backtrace();
+    }
+    else
+    {
+        fprintf( stderr, "Matching test is not exist.\n" );
+        exit( -1 );
+    }
+
+    return 0;
+}
+
+void sub_func( int );
+void run_basic_backtrace()
+{
+    sub_func( 5 );
+}
---
+++ test/stack_backtrace_operation/with_test.cpp
@@ -0,0 +1,22 @@
+#include "stack_backtrace_operation"
+
+#include <cstdlib>
+#include <iostream>
+
+
+void subsub_func()
+{
+    std::cout << "stack_trace\n";
+    std::cout << boost::stacktrace::stacktrace();
+    exit( EXIT_SUCCESS );
+}
+
+void sub_func( int level )
+{
+    if ( 0 < level )
+    {
+        sub_func( level - 1 );
+        return;
+    }
+    subsub_func();
+}
